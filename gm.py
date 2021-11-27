6a160aa6cf0123db302057b0a23534e749ad2f5b
蠢丶 <zzchun12826@gmail.com>
2021-11-27 19:35:39 +0800

    update

--- gm.py
+++ gm.py
@@ -8,11 +8,12 @@ import plotext as plt
 data = defaultdict(list)
 argv_len = len(sys.argv)
 url = sys.argv[1] if argv_len > 1 and sys.argv[1] else 'http://localhost:51516'
-interval_ = max(int(sys.argv[2]), 5) if argv_len > 2 and sys.argv[1] else 5
-range_ = max(int(sys.argv[3]), 12) if argv_len > 3 and sys.argv[1] else 12
+interval_ = max(int(sys.argv[2]), 5) if argv_len > 2 and sys.argv[2] else 5
+range_ = max(int(sys.argv[3]), 12) if argv_len > 3 and sys.argv[3] else 12
+stats_ = sys.argv[4] if argv_len > 4 and sys.argv[4] else 'slow_query_latency_us.avg.5,slow_query_latency_us.p99.5'
 
 def fetch():
-    r = requests.get(f'{url}/stats?stats=slow_query_latency_us.avg.5,slow_query_latency_us.p99.5&format=json')
+    r = requests.get(f'{url}/stats?stats={stats_}&format=json')
     for d in r.json():
         for k, v in d.items():
             data[k].append(v)
@@ -22,7 +23,7 @@ def monitor():
     plt.title("Nebula Monitor")
     len_ = len(data.keys())
     if len_:
-        plt.subplots(int(len_/2), 2)
+        plt.subplots(max(1, int(len_/2)), 2)
         for i, (k, v) in enumerate(data.items()):
             plt.subplot(int(i/2)+1, i%2+1)
             plt.title(k)
