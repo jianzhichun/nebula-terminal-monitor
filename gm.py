22ff488562b9d2e0b54a83e3fabe8a3f7476dd94
蠢丶 <zzchun12826@gmail.com>
2021-11-27 19:27:04 +0800

    first commit

---
+++ gm.py
@@ -0,0 +1,43 @@
+import sys
+import time
+from collections import defaultdict
+import schedule
+import requests
+import plotext as plt
+
+data = defaultdict(list)
+argv_len = len(sys.argv)
+url = sys.argv[1] if argv_len > 1 and sys.argv[1] else 'http://localhost:51516'
+interval_ = max(int(sys.argv[2]), 5) if argv_len > 2 and sys.argv[1] else 5
+range_ = max(int(sys.argv[3]), 12) if argv_len > 3 and sys.argv[1] else 12
+
+def fetch():
+    r = requests.get(f'{url}/stats?stats=slow_query_latency_us.avg.5,slow_query_latency_us.p99.5&format=json')
+    for d in r.json():
+        for k, v in d.items():
+            data[k].append(v)
+            data[k] = data[k][-1:-11:-1]
+
+def monitor():
+    plt.title("Nebula Monitor")
+    len_ = len(data.keys())
+    if len_:
+        plt.subplots(int(len_/2), 2)
+        for i, (k, v) in enumerate(data.items()):
+            plt.subplot(int(i/2)+1, i%2+1)
+            plt.title(k)
+            plt.cld()
+            plt.clt()
+            plt.clc()
+            plt.datetime.set_datetime_form(date_form = "%Y-%m-%d %H:%M:%S")
+            curr = time.time()
+            x = [time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(curr - interval_*i)) for i in range(0, range_)][::-1]
+            plt.plot_date(x, v, marker = "dot")    
+        plt.show()
+
+if __name__ == '__main__':
+    schedule.every(interval_).seconds.do(fetch)
+    schedule.every(1).seconds.do(monitor)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
