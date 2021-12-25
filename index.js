167bc27e9e637e0c58328039c1eca3f75555625b
蠢丶 <zzchun12826@gmail.com>
2021-12-25 16:25:36 +0800

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const list2pairs = (arr) => arr.reduce(function (result, value, index, array) {
   return result;
 }, [])
 const subArgs = `${args.node ? "-n " : " "}${args.graph ? "-g " : " "}${args.https ? "-https " : " "}${args.host ? "-h " + args.host + " " : " "}${args.port ? "-p " + args.port + " " : " "}`
-let command = list2pairs(args.stats).map(pair => "[" + pair.map(stat => ` -t '${stat.replace(/[_\.]/g, " ")}' "node src/chart/linechart ${subArgs}-s ${stat}"`).join(" .. ") + "]").join(" : ")
+let command = list2pairs(args.stats).map(pair => " [ " + pair.map(stat => ` -t '${stat.replace(/[_\.]/g, " ")}' "node src/chart/linechart ${subArgs}-s ${stat}"`).join(" .. ") + " ] ").join(" : ")
 execSync(
   `./node_modules/.bin/stmux -M -w always -e ERROR -m beep -- [ ${command} ]`,
   { stdio: "inherit" }
