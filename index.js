beee9ac6da421bd9e5bae5e18efb2108a9b2d5ce
蠢丶 <zzchun12826@gmail.com>
2021-12-25 15:24:11 +0800

    update

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
 #!/usr/bin/env node
 
 import args from "./src/argument.js"
-import ervy from "ervy"
 import stats from "./src/stats.js"
-import moment from "moment"
-import { normalize, time, interpolate, zip } from "./src/utils.js"
-const { bar, pie, bullet, donut, gauge, scatter, fg, bg } = ervy
+import asciichart from "asciichart";
+const screenHeight = process.stdout.rows;
 
 try {
-  let data = []
+  let data = new Array(120).fill(0)
   setInterval(() => {
     stats().then(map => {
       if (data.length > 120) {
         data.pop()
       }
-      data.push(map)
+      data.push(map[args.stat])
     })
-  }, 100)
+  }, 1000)
   setInterval(() => {
-    stats().then(map => {
-      console.clear()
-      const pieData1 = [
-        { key: 'A', value: 5, style: '* ' },
-        { key: 'B', value: 10, style: '+ ' },
-        { key: 'C', value: 10, style: '# ' },
-        { key: 'D', value: 10, style: 'O ' }
-      ]
-
-      const pieData2 = [
-        { key: 'A', value: 5, style: bg('cyan', 2) },
-        { key: 'B', value: 5, style: bg('yellow', 2) },
-        { key: 'C', value: 5, style: bg('magenta', 2) },
-        { key: 'D', value: 5, style: bg('white', 2) }
-      ]
-      zip(
-        pie(pieData1, { left: 1 }).split(/\r?\n/),
-        pie(pieData2, { left: 100 }).split(/\r?\n/),
-        (a1, a2) => console.log(a1 + a2)
-      )
-    })
-  }, 100);
+    console.clear()
+    console.log(asciichart.plot(data, { height: screenHeight - 4 }))
+  }, 100)
 } catch (e) {
   console.log("Couldn't plot chart. Please try different settings.")
   process.exit(1)
--- src/stats.js
+++ src/stats.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 import args from "./argument.js"
 
 
-export default function () {
+export default function (stats = "num_queries.rate.5") {
     return axios({
         method: 'GET',
-        url: `${args.https ? "https" : "http"}://${args.host}:${args.port}/stats?format=json`
+        url: `${args.https ? "https" : "http"}://${args.host}:${args.port}/stats?format=json${stats ? "&stats=" + stats : ""}`
     }).then(({ data }) => data.reduce(function (map, obj) {
         let key = Object.keys(obj)[0]
         map[key] = obj[key]
