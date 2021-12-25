0e05640b6d13f9e38b2205b725a13b97246873ea
zexuan.zzx <zexuan.zzx@alibaba-inc.com>
2021-12-25 13:56:59 +0800

    feat: add charts

--- chart/a.js
+++ chart/a.js
@@ -1,11 +1,13 @@
 // 折线图
-import asciichart from "asciichart"
+import asciichart from "asciichart";
 
 const screenWidth = process.stdout.columns;
 const screenHeight = process.stdout.rows;
-const s0 = new Array(screenWidth);
-for (let i = 0; i < s0.length; i++)
-  s0[i] = process.stdout.rows/5 * Math.sin(i * ((Math.PI * 4) / s0.length));
-console.log(asciichart.plot(s0));
-
-process.stdin.resume();
+const maxLen = screenWidth - 15;
+const s0 = new Array(maxLen).fill(0);
+setInterval(() => {
+  console.clear();
+  if (s0.length > maxLen) s0.shift();
+  s0.push(Math.random());
+  console.log(asciichart.plot(s0, { height: screenHeight - 4 }));
+}, 500);
